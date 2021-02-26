4d3870fcc64d5d344e0c3fc9f7e527e0c09945bd
Lívia <leave.ah@gmail.com>
2021-02-26 17:48:57 -0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import './assets/scss/reset.scss'
+import Time from './ds/Time.vue'
 
+Vue.config.productionTip = false
+Vue.component('Time', Time)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
