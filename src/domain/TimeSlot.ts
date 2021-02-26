ea0d576f026446cf698ab7420e1dc9fe721e32db
Lívia <leave.ah@gmail.com>
2021-02-26 17:54:24 -0300

    Create TimeSlot.ts

---
+++ src/domain/TimeSlot.ts
@@ -0,0 +1,41 @@
+export default class TimeSlot {
+    hour: number
+    minute: number
+
+    constructor(hour = -1, minute = -1) {
+        this.hour = hour
+        this.minute = minute
+    }
+
+    static compare(a: TimeSlot, b: TimeSlot) {
+        if (a.hour < b.hour) return -1
+        if (a.hour > b.hour) return 1
+
+        if (a.minute < b.minute) return -1
+        return 1
+    }
+
+    add(time: TimeSlot): TimeSlot {
+        const mm = this.minute + time.minute
+        const hh = this.hour + time.hour + Math.floor(mm / 60)
+
+        const minute = mm % 60
+        const hour = hh % 24
+
+        return new TimeSlot(hour, minute)
+    }
+
+    subtract(time: TimeSlot): TimeSlot {
+        const mm = this.minute - time.minute
+        const hh = this.hour - time.hour - (mm < 0 ? 1 : 0)
+
+        const minute = mm >= 0 ? mm : 60 + mm
+        const hour = hh >= 0 ? hh : 24 + hh
+
+        return new TimeSlot(hour, minute)
+    }
+
+    toString() {
+        return `${this.hour}:${this.minute}`
+    }
+}
