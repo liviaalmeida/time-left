852b47c72a69c9baf9671a806a8092c9645cdd04
Lívia <leave.ah@gmail.com>
2021-02-26 17:54:46 -0300

    Create Time.vue

---
+++ src/ds/Time.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="time">
+        <input type="time"
+        @input="onTime($event.target.value)">
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TimeSlot from '@/domain/TimeSlot'
+
+export default Vue.extend({
+    props: {
+        time: {
+            type: Object as () => TimeSlot,
+            required: false,
+        }
+    },
+    methods: {
+        onTime(time: string) {
+            const [ hour, minute ] = time.split(':').map(num => Number(num))
+            this.$emit('time', new TimeSlot(hour, minute))
+        },
+    },
+})
+</script>
+
+<style lang="scss">
+.time {
+    display: inline-block;
+}
+</style>
