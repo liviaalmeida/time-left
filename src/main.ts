a3bde4faab11df8780264862b1bda97a2fe5967c
Lívia Almeida <leave.ah@gmail.com>
2021-03-05 18:12:37 -0300

    Removed reset from main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import './assets/scss/reset.scss'
 import Time from './ds/Time.vue'
 
 Vue.config.productionTip = false
